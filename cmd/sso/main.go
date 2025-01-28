0d8241e6c1a675b1b1bb3bb58440cefb61890e85
Nikolay <Cherashev4@gmail.com>
2025-01-28 11:06:53 +0500

    Create project

---
+++ cmd/sso/main.go
@@ -0,0 +1,46 @@
+﻿package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting app", slog.Any("config", cfg))
+
+	// TODO: инициализировать приложение (app)
+
+	// TODO: запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return logger
+}
---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,7 @@
+﻿package auth
+
+import ssov1 "sso/gen/go/sso"
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+}
